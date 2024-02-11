37738750b5901ab6148bf3a357ed150e56d98f8b
nabin <nabin123456j@gmail.com>
2024-02-11 23:11:26 +0545

    button is made

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,104 @@
+import React, { useEffect, useState } from 'react'
+
 import './App.css'
 import AddNew from './Component/AddNew'
+import Search from './Component/Search'
+
+
+import { Button } from '@mantine/core';
+
+
 
 function App() {
 
+  const [display, setDisplay] = useState(true);
+
+  const txt: string = "abcdefghij"
+  const txtLength: number = txt.length
+
+
+  const setHandle = (e) => {
+    setDisplay(!display)
+  }
+
+  const HiddenNumber: object = {
+    4: 2,
+    5: 3,
+    6: 4,
+    7: 4,
+    8: 5,
+    9: 6,
+    10: 7,
+    // 11: (txtLength > 10 ? 9 : 1)
+  }
+
+  let a: number;
+
+  for (const key in HiddenNumber) {
+    if (txtLength == +key) {
+      a = HiddenNumber[key]
+    } else if (txtLength < 4) {
+      a = 1
+    } else if (txtLength > 10) {
+      a = 10
+    }
+  }
+
+
+
+  const myArr: [] = [];
+
+  for (let i = 1; i <= a; i++) {
+    myArr.push(Math.floor(Math.random() * txtLength))
+  }
+
+
+  // console.log(myArr);
+  function test(txt, myArr) {
+
+    let arr = txt.split('');
+    console.log(myArr);
+
+    console.log(arr);
+
+    for (let a of myArr) {
+      console.log(arr[a]);
+      arr[a] = "*"
+    }
+
+
+
+    return arr.join('')
+  }
+
+
+  const finaltxt = test(txt, myArr);
+
+
   return (
-    <>
-      <div className='h-screen w-screen w-full grid place-content-center bg-blue-900 relative'>
-        <div id="center" className='absolute bg-blue-300 rounded-2xl px-2 w-3/4 h-8 m-5 left-2/4 -translate-x-2/4 flex justify-between items-center'>
-          <AddNew />
-          <AddNew />
+    <div className='relative '>
+      <div className='absolute z-50 top-7 w-full flex justify-between px-4 '>
+        <AddNew />
+
+        <Search />
+      </div>
+      <div className='h-screen w-screen grid place-content-center bg-blue-900 relative'>
+        <div className='text-white '>
+          {/* <p className='mb-9 text-center text-xs' >Word for You 🔥</p> */}
+          <p className='inline-block p-3 px-9 rounded-xl tracking-wider bg-blue-950 shadow-lg'>
+            {display ? finaltxt : txt}
+          </p>
+
+          <Button
+            className='text-sm bg-blue-950  rounded-xl ml-3 hover:bg-blue-500 hover:shadow-xl transition-colors border-blue-500 border-2'
+            onClick={setHandle}
+          >
+            Reveal
+          </Button>
         </div>
 
-        <div>
-          <span className='inline-block p-3 px-9 rounded-xl tracking-wider bg-blue-950 text-white'>NabinBhandari</span>
-        </div>
       </div>
-    </>
+    </div>
   )
 }
 
--- src/Component/Search.tsx
+++ src/Component/Search.tsx
@@ -1,16 +1,16 @@
 import { useDisclosure } from '@mantine/hooks';
 import { Modal, Button } from '@mantine/core';
 
-function AddNew() {
+function Search() {
     const [opened, { open, close }] = useDisclosure(false);
-  
+
     return (
         <>
             <Modal
                 opened={opened}
                 onClose={close}
                 title="test"
-              
+
             >
                 {/* Modal content */}
             </Modal>
@@ -19,8 +19,9 @@ function AddNew() {
             <Button
                 onClick={open}
             >
-                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" className="bi bi-plus-circle-fill" viewBox="0 0 16 16">
-                    <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0M8.5 4.5a.5.5 0 0 0-1 0v3h-3a.5.5 0 0 0 0 1h3v3a.5.5 0 0 0 1 0v-3h3a.5.5 0 0 0 0-1h-3z" />
+                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" className="bi bi-search-heart" viewBox="0 0 16 16">
+                    <path d="M6.5 4.482c1.664-1.673 5.825 1.254 0 5.018-5.825-3.764-1.664-6.69 0-5.018" />
+                    <path d="M13 6.5a6.47 6.47 0 0 1-1.258 3.844q.06.044.115.098l3.85 3.85a1 1 0 0 1-1.414 1.415l-3.85-3.85a1 1 0 0 1-.1-.115h.002A6.5 6.5 0 1 1 13 6.5M6.5 12a5.5 5.5 0 1 0 0-11 5.5 5.5 0 0 0 0 11" />
                 </svg>
             </Button>
         </>
@@ -28,4 +29,4 @@ function AddNew() {
 
     )
 }
-export default AddNew
+export default Search
